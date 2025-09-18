f664e26974ba096c60f85aa7eed0dc3939077518
GitHub Action <action@github.com>
2025-09-18 06:34:33 +0000

    Bump version to 2.6.4 (206004)

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.11.16"
-            versionCode = 211016
+            versionName = "2.6.4"
+            versionCode = 206004
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
